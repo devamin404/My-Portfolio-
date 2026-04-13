53bca4e767bbeff017d23513ce22fbea37c24094
Muhammad Amin <aminumer724@gmail.com>
2026-04-13 15:04:53 +0500

    style: modified header design and completed the section

---
+++ src/Components/Header.jsx
@@ -0,0 +1,37 @@
+import styles from "./Header.module.css";
+import myPic from "../Assets/img/Amin 3.png";
+
+function Header() {
+  return (
+    <header id="home">
+      <nav className="container">
+        <a href="#home">Home</a>
+        <a href="#about">About</a>
+        <a href="#work">Work</a>
+        <a href="#contact">Contact</a>
+      </nav>
+
+      <section className={`container ${styles.hero}`}>
+        <div className={styles.heroContent}>
+          <h1 className="lgHeading">Hi! I'm Amin</h1>
+          <h2 className="mdHeading">Front End Developer</h2>
+
+          <p>
+            A self-driven front-end developer specializing in building
+            responsive and performance-optimized web applications. I'm
+            passionate about clean code, user-centric design, and creating
+            digital solutions that enhance user engagement.
+          </p>
+
+          <button className="btn1">Let's Connect</button>
+        </div>
+
+        <div className={styles.heroImg}>
+          <img src={myPic} alt="My Picture" className="myImg" />
+        </div>
+      </section>
+    </header>
+  );
+}
+
+export default Header;
