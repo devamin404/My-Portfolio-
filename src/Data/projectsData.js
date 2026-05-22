d9ffaa0d646e4ffca10f71e8a9abcfb274efc5ba
Muhammad Amin <aminumer724@gmail.com>
2026-05-22 11:18:58 +0500

    refactor: removed duplicate todo project in portfolio

--- src/Data/projectsData.js
+++ src/Data/projectsData.js
@@ -10,17 +10,6 @@ const projectsList = [
     liveLink: "https://todo-app-tau-opal-20.vercel.app/",
     linkedinLink: "https://www.linkedin.com/in/muhammad-amin-989269398",
   },
-  {
-    id: 2,
-    title: "Todo List",
-    description:
-      "A responsive React Todo app with real-time tracking, smart filters, and persistent storage, featuring inline editing and keyboard shortcuts.",
-    image:
-      "https://img.freepik.com/premium-photo/vector-style-illustration-cartoon-paper-pencil-with-todo-list-idea-thought-bubble_845114-9027.jpg?w=2000",
-    gitHubLink: "https://github.com/devamin404/Todo-App.git",
-    liveLink: "https://todo-app-tau-opal-20.vercel.app/",
-    linkedinLink: "https://www.linkedin.com/in/muhammad-amin-989269398",
-  },
 ];
 
 export default projectsList;
